4298bf0418fb2a0ff306ee2bd6ce987aaac873fb
Cesar Muñoz <cesar.alberto.munoz@gmail.com>
2022-07-21 11:35:40 +0200

    fix: dependencies

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { Application, Router, isHttpError, Status } from "https://deno.land/x/oak/mod.ts";
+export { Application, Router, isHttpError, Status } from "https://deno.land/x/oak@v10.6.0/mod.ts";
